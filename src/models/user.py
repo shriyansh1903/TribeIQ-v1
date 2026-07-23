8a9887703cb3ca4a498f4583a4e4a59d3acf5a18
shriyansh1903 <shriyansh.malik8841@gmail.com>
2026-07-23 15:25:29 +0530

    fix(deps): add email-validator to requirements.txt and use optional str in user model

--- src/models/user.py
+++ src/models/user.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, Field
 from typing import Optional
 
 class User(BaseModel):
     user_id: str = Field(alias="User ID")
     username: str
     password_hash: str
-    email: Optional[EmailStr] = None
+    email: Optional[str] = None
     role: str = "Read Only"
     status: str = "Active"
 
